8a688d265427d881ad73e61615536424beada7c1
VAKilyakov <vakilyakov@bk.ru>
2023-07-23 15:32:32 +0300

    Task 2 is done v1

--- Program.cs
+++ Program.cs
@@ -79,3 +79,86 @@ PS D:\Home_Works\CS_HW_6> dotnet run
 Ваш массив [1,-7,567,89,223]В Вашем массиве больше нуля 4 чисел
 */
 
+/* Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+начения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+/* Решение 2
+const int Coeficent = 0;
+const int Constante =1;
+const int X_Coordinate = 0;
+const int Y_Coordinate = 1;
+const int Line_1 = 1;
+const int Line_2 = 2;
+
+Double InputNum(string message) //Общение с пользователем на прием чисел
+{
+    
+    Console.Write(message + " ");
+    return Convert.ToDouble(Console.ReadLine());
+
+}
+
+Double[] InputLineDate(int NumberOfLine) //Ввод К и Б для прямой
+{
+    double[] LineDate = new double[2];
+
+    LineDate[0] = InputNum("Введите значение Б для " + NumberOfLine);
+    LineDate[1] = InputNum("Введите значение кофициента К для " + NumberOfLine);
+
+    return LineDate;
+}
+
+bool ValidateLine (double[] LineDate_1, double[] LineDate_2)
+{
+    
+    bool Result = true;
+    
+    if (LineDate_1[Coeficent] == LineDate_2[Coeficent])
+    {
+        if (LineDate_1[Constante] == LineDate_2[Constante])
+        {
+             Console.Write("Прямые совпадают");
+             Result = false;   
+        }
+        else
+        {
+             Console.Write("Прямые параллельны");
+             Result = false; 
+        }
+    }
+
+    return Result;
+}
+
+Double[] FindCoordinate (double[] LineDate1, double[] Linedate2)
+{
+    double[]Coodrinate = new double[2];
+
+    Coodrinate[X_Coordinate] = (LineDate1[Coeficent]-Linedate2[Coeficent])/(LineDate1[Constante]-Linedate2[Constante]);
+    Coodrinate[Y_Coordinate] = LineDate1[Coeficent]*Coodrinate[X_Coordinate] - LineDate1[Constante];
+
+    return Coodrinate;
+}
+
+double[] LineDate_1 = InputLineDate(Line_1);
+double[] LineDate_2 = InputLineDate(Line_2);
+
+if (ValidateLine(LineDate_1,LineDate_2))
+{
+     double[] Coordinate = FindCoordinate(LineDate_1,LineDate_2);
+     Console.Write($"Точка пересечения прямых заданных уравнениями Y = {LineDate_1[Constante]} * X - {LineDate_1[Coeficent]} и Y = {LineDate_2[Constante]} * X - {LineDate_2[Coeficent]}");
+     Console.Write($" имеет значения Y = {Coordinate[Y_Coordinate]} и X = {Coordinate[X_Coordinate]}");
+}
+*/
+
+/* Тест 2
+PS D:\Home_Works\CS_HW_6> dotnet run
+Введите значение Б для 1 2
+Введите значение кофициента К для 1 5
+Введите значение Б для 2 4
+Введите значение кофициента К для 2 9
+Точка пересечения прямых заданных уравнениями Y = 5 * X - 2 и Y = 9 * X - 4 имеет значения Y = 6 и X = 0,5
+*/
+
